d8c0667cc4ad48495bd6f876de31fa0880049c66
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-01 16:06:59 +0300

    Initial Commit

---
+++ Quote Machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quote Machine
+//
+//  Created by Rinalds Domanovs on 01/08/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
