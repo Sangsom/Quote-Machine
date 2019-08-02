b4cb76818b87cc576f3862ec92723d2e3ca88918
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-02 13:14:33 +0300

    Resetting initial code

--- Quote Machine/ViewController.swift
+++ Quote Machine/ViewController.swift
@@ -12,24 +12,10 @@ class ViewController: UIViewController {
     
     @IBOutlet var quoteLabel: UILabel!
     @IBOutlet var authorLabel: UILabel!
-    var quotes = [Quote]()
-
-    var quoteController = QuoteController()
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        quoteController.fetchQuote { (quotes) in
-            if let quotes = quotes {
-                DispatchQueue.main.async {
-                    self.quotes = quotes
-                }
-            }
-        }
 
-
-        print(quotes)
     }
 
 
