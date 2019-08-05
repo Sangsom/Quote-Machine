5b12194303faf6ce9a038034069586af46723c36
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-05 12:49:58 +0300

    Added CategoriesTableView and linked to storyboard

---
+++ Quote Machine/CategoriesTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesTableViewController.swift
+//  Quote Machine
+//
+//  Created by Rinalds Domanovs on 05/08/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
