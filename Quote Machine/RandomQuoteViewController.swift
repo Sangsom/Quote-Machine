15efcb28e555d8e5a65be8a0a0f5e9927a83496d
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-05 14:48:48 +0300

    Added Random Quote VC and linked to storyboard

---
+++ Quote Machine/RandomQuoteViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RandomQuoteViewController.swift
+//  Quote Machine
+//
+//  Created by Rinalds Domanovs on 05/08/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class RandomQuoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
