9c19c66d5cb03e0785f2c1f700d318d91b42d37e
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-05 14:49:55 +0300

    Added navigation and navigation title to RandomQuoteVC

--- Quote Machine/RandomQuoteViewController.swift
+++ Quote Machine/RandomQuoteViewController.swift
@@ -13,7 +13,7 @@ class RandomQuoteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Random Quote"
     }
 
 }
